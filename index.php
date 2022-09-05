ff8ead84b7f506aa5a34dd5c28b6ecb2053eb6d2
Andrea Prina <and.prina@gmail.com>
2022-09-05 17:25:55 +0200

    Add discount application based on user type

--- index.php
+++ index.php
@@ -18,10 +18,12 @@
         
         $cart1 = new ShoppingCart();
         
-        $product1 = new Product("Croccantini 1kg", 23.50);
-        $product2 = new Product("Cuccia", 54.00);
+        $product1 = new Product("Croccantini 1kg", 50.00);
+        $product2 = new Product("Cuccia", 50.00);
         
         $user1 = new RegisteredUser("Federica", "Massoletti", "Via Costanza, 2 20100 Milano", $cart1, "fm@mail.com", "12345", 20);
+
+        // $user1 = new User("Federica", "Massoletti", "Via Costanza, 2 20100 Milano", $cart1);
         
         // var_dump($cart1);
         
@@ -32,7 +34,14 @@
         
         var_dump($user1);
 
-        echo "TOTAL PRICE: " . $user1->shoppingCart->getTotalPrice();
+        $amountToPay = 0;
+        if (is_a($user1, "RegisteredUser")){
+            $amountToPay = $user1->shoppingCart->getTotalPrice() * (1- ($user1->getDiscountPercentage() / 100));
+        } else {
+            $amountToPay = $user1->shoppingCart->getTotalPrice();
+        }
+
+        echo "TOTAL AMOUNT TO PAY: " . $amountToPay;
 
     ?>
 
